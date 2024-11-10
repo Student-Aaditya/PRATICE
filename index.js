c386773cb3259c7e1c6d209c4d75077e709aa43d
Aaditya <aadityakuamr518@gmail.com>
2024-11-10 21:21:10 +0530

    add

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ app.set("views", path.join(__dirname, "view"));
 app.set("view engine", "ejs");
 
 app.get("/",(req,res)=>{
-    res.render("index.ejs");
+    // res.render("index.ejs");
+    res.send("hello");
 })
 app.listen(port,(req,res)=>{
     console.log(`server working on ${port}`);
